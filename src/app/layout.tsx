4223e40c53ed834245a4a74ddcc791bd6643d5e2
olorenzoj <olorenzo2004@gmail.com>
2025-10-16 13:50:20 -0500

    added next auth with google oauth 2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,30 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/ui/Header'
 import { Toaster } from 'react-hot-toast'
+import Providers from './providers' // 👈 lo agregamos
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Gestión Torneos Tenis de Mesa',
-  description: 'Sistema para administrar torneos de tenis de mesa',
-  icons: '/logo.jpg'
+    title: 'Gestión Torneos Tenis de Mesa',
+    description: 'Sistema para administrar torneos de tenis de mesa',
+    icons: '/logo.jpg',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="es">
-      <body className={`${inter.className} bg-gray-50`}>
-        <Header />
-        <main className="container mx-auto p-4">
-          {children}
-        </main>
-        <Toaster position="top-right" />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="es">
+        <body className={`${inter.className} bg-gray-50`}>
+        <Providers> {/* ✅ Envolvemos toda la app */}
+            <Header />
+            <main className="container mx-auto p-4">{children}</main>
+            <Toaster position="top-right" />
+        </Providers>
+        </body>
+        </html>
+    )
 }
