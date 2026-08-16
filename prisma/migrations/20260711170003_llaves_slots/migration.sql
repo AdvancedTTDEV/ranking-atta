0ff297bfbb659424e31aa0ae975ff296bdf560c2
Lorenzojj <olorenzo2004@gmail.com>
2026-08-16 17:11:08 -0500

    DELETE MIGRATIONS

--- prisma/migrations/20260711170003_llaves_slots/migration.sql
+++
@@ -1,7 +0,0 @@
--- Casillas vacías y encadenamiento de llaves de eliminación.
-ALTER TABLE `torneo_partidos_programados`
-  MODIFY COLUMN `participante_local_id` INT NULL,
-  MODIFY COLUMN `participante_visitante_id` INT NULL,
-  ADD COLUMN `ronda_eliminacion` VARCHAR(30) NULL,
-  ADD COLUMN `posicion_llave` INT NULL,
-  ADD KEY `tpp_ronda_llave_idx` (`ronda_eliminacion`, `posicion_llave`);
